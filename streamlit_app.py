de241aa03bceb21f7232c464a8bc767acc93a183
Mthabisincube <mthabisincube90@gmail.com>
2025-11-20 12:35:44 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit as st
+import pandas as pd 
 
 st.title('Smart Credit App')
-
 st.write('Taking Credit Scoring to the next level')
+
+df = pd.read_csv("https://raw.githubusercontent.com/Mthabisincube/Credit-Smart-project/refs/heads/master/smart_credit_scoring_zimbabwe.csv")
+df
