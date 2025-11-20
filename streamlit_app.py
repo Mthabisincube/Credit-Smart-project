3bd126d8ae916b44cced8dd8f70cc72e86102cd9
Mthabisincube <mthabisincube90@gmail.com>
2025-11-20 12:54:37 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,21 @@ st.write('Taking Credit Scoring to the next level')
 df = pd.read_csv("https://raw.githubusercontent.com/Mthabisincube/Credit-Smart-project/refs/heads/master/smart_credit_scoring_zimbabwe.csv")
 
 with st.expander('Data'):
-    st.write('Raw Data')
+    st.write('**Raw Data**')
     st.write(df)
+
+st.write('**X**')
+X = df.drop ("Credit_Score", axis=1)
+X
+
+st.write('**Y**)
+Y = df.Credit_Score
+Y
+
+         
+         
+
+
+
+
 
